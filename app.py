60b82a1a378145320a130c3938dc603a1d705e2b
beanbeansprout <55658099+beanbeansprout@users.noreply.github.com>
2023-10-17 13:38:19 -0400

    Generate stock statements

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import product
 
 product1 = product.Product(int(input('Enter product code: ')), input('Enter product name: '), float(input ('Enter product sale price: ')), float(input('Enter product manufacture cost: ')), int(input('Enter stock level: ')), int(input('Enter monthy units manufactured: ')))
+
+product1.printDetails()
+
+for i in range(1, 13):
+    product1.generateStockStatement(i)
--- product.py
+++ product.py
@@ -1,3 +1,5 @@
+import random
+
 class Product:
     def __init__(self, code, name, price, manufactureCost, stockLevel, monthlyUnitsManufactured):
         self._code = code
@@ -6,5 +8,25 @@ class Product:
         self._manufactureCost = manufactureCost
         self._stockLevel = stockLevel
         self._monthlyUnitsManufactured = monthlyUnitsManufactured
+
+    def printDetails(self):
+        print('\nProduct Code:', self._code)
+        print('Product Name: ' + self._name, '\n')
+        print('Sale Price:', self._price, 'CAD')
+        print('Manufacture Cost:', self._manufactureCost, 'CAD')
+        print('Monthly Production:', self._monthlyUnitsManufactured, 'units (Approx.)\n')
     
-    
+    def getMonthlyUnitsSold(self):
+        return self._monthlyUnitsManufactured + random.randrange(-10, 11)
+
+    def updateStockLevel(self, unitsSold):
+        self._stockLevel += self._monthlyUnitsManufactured - unitsSold
+
+    def generateStockStatement(self, index):
+        monthlyUnitsSold = self.getMonthlyUnitsSold()
+        self.updateStockLevel(monthlyUnitsSold)
+
+        print('Month', index, ': ')
+        print('|    Manufactured:', self._monthlyUnitsManufactured, 'units')
+        print('|    Sold:', monthlyUnitsSold)
+        print('|    Stock:', self._stockLevel)

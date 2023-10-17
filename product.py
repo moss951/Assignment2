17d9685e564b96a792bf6860039bfa8a97ec2df7
beanbeansprout <55658099+beanbeansprout@users.noreply.github.com>
2023-10-17 14:09:17 -0400

    created stockStatement class for organization

--- product.py
+++ product.py
@@ -1,41 +1,11 @@
 import random
 
 class Product:
-    def __init__(self, code, name, price, manufactureCost, stockLevel, monthlyUnitsManufactured):
+    def __init__(self, code, name):
         self._code = code
         self._name = name
-        self._price = price
-        self._manufactureCost = manufactureCost
-        self._stockLevel = stockLevel
-        self._monthlyUnitsManufactured = monthlyUnitsManufactured
-
-        self._totalUnitsSold = 0
-        self._totalUnitsManufactured = 0
 
     def printDetails(self):
         print('\nProduct Code:', self._code)
         print('Product Name: ' + self._name, '\n')
-        print('Sale Price:', self._price, 'CAD')
-        print('Manufacture Cost:', self._manufactureCost, 'CAD')
-        print('Monthly Production:', self._monthlyUnitsManufactured, 'units (Approx.)\n')
-    
-    def getMonthlyUnitsSold(self):
-        return self._monthlyUnitsManufactured + random.randrange(-10, 11)
-
-    def updateStockLevel(self, unitsSold):
-        self._stockLevel += self._monthlyUnitsManufactured - unitsSold
-
-    def generateStockStatement(self, index):
-        monthlyUnitsSold = self.getMonthlyUnitsSold()
-        self.updateStockLevel(monthlyUnitsSold)
-
-        print('Month', index, ': ')
-        print('|    Manufactured:', self._monthlyUnitsManufactured, 'units')
-        print('|    Sold:', monthlyUnitsSold)
-        print('|    Stock:', self._stockLevel)
-
-        self._totalUnitsManufactured += self._monthlyUnitsManufactured
-        self._totalUnitsSold += monthlyUnitsSold
-
-    def getNetProfit(self):
-        return self._totalUnitsSold * self._price - self._totalUnitsManufactured * self._manufactureCost
+        

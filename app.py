233d74fa17f7c2c4ea898d4a895d03450284992f
beanbeansprout <55658099+beanbeansprout@users.noreply.github.com>
2023-10-17 13:03:48 -0400

    get inputs from user

---
+++ app.py
@@ -0,0 +1,7 @@
+productCode = int(input('Enter product code: '))
+productName = input('Enter product name: ')
+productPrice = float(input ('Enter product sale price: '))
+productManufactureCost = float(input('Enter product manufacture cost: '))
+stockLevel = int(input('Enter stock level: '))
+monthlyUnitsManufactured = int(input('Enter monthy units manufactured: '))
+
